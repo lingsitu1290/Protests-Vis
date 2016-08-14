a4fa2c62d8cb5183aec7fdbde4b531c8595b977c
Ling <lingsitu1290@gmail.com>
2016-08-14 01:42:08 +0000

    Created Nav Bar.

--- static/javascript/heatmap.js
+++ static/javascript/heatmap.js
@@ -7,6 +7,48 @@ function initialize() {
              minZoom: 2
           };
           var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
        }
 
        google.maps.event.addDomListener(window, 'load', initialize);
+
+
+////////////
+// marker //
+////////////
+
+// function addMarker() {
+//   var myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+//   var image = myImageURL;
+//   var nearSydney = new google.maps.LatLng(-34.788666, 150.41146)
+
+// addMarker()
+
+
+ ///////////////
+ // basic map //
+ ///////////////
+
+
+////////////
+// marker //
+////////////
+
+function addMarker() {
+  var myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+  var image = myImageURL;
+  var nearSydney = new google.maps.LatLng(-34.788666, 150.41146)
+  var marker = new google.maps.Marker({
+      position: nearSydney,
+      map: map,
+      title: 'Hover text',
+      icon: image
+  });
+  return marker;
+}
+
+var marker = addMarker();
+
+/////////////////
+// info window //
+/////////////////
