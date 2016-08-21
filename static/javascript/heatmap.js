90e647d211f22f99bf27817311b69923b77724e3
Ling <lingsitu1290@gmail.com>
2016-08-21 20:43:10 +0000

    Able to slide through 8/01/2016 to 8/18/2016.

--- static/javascript/heatmap.js
+++ static/javascript/heatmap.js
@@ -122,10 +122,32 @@ function initialize() {
 
             map.mapTypes.set('map_style', customMapType);
             map.setMapTypeId('map_style');
+
+// get list of dates
+function getListOfDates(){
+    var arrayOfDates = [];
+    $.get('/events', function (events){
+        var events;
+        // var arrayOfDates = [];
+
+        for (var key in events) {
+            var one_event = events[key]
+
+            arrayOfDates.push(parseInt(one_event.fullDate));
+    }
+    console.log(arrayOfDates)
+
+});
+    // console.log(Array.isArray(arrayOfDates))
+    return arrayOfDates;
+}
+
+getListOfDates();
+
 /* Add Slider */ 
 
 function createSlider(){
-    var sliderDate = [20160801,20160802,20160803,20160804,20160805,20160806]
+    var sliderDate = [20160801,20160802,20160803,20160804,20160805,20160806,20160807, 20160808,20160809,20160810,20160811,20160812,20160813,20160814,20160815,20160816, 20160817, 20160818]
     $("#slider-1").slider({
         min: sliderDate[0],
         max: sliderDate[sliderDate.length-1],
@@ -151,25 +173,6 @@ function createSlider(){
 
 createSlider();
 
-// get list of dates
-function getListOfDates(){
-    $.get('/events', function(events){
-        var events;
-        var arrayOfDates = [];
-
-        for (var key in events) {
-        //     arrayOfDates.push(events[key].fullDate);
-            var one_event = events[key]
-
-            arrayOfDates.push(one_event.fullDate);
-    }
-    console.log(arrayOfDates);
-});
-}
-
-getListOfDates();
-
-
 // Retrieving events information with AJAX
 function changeMap(fullDate){
     $.get('/events/' + fullDate + '.json', function (events) {
@@ -180,18 +183,8 @@ function changeMap(fullDate){
 
         for (var key in events) {
             var one_event = events[key]
-            // var latitude = parseInt(one_event.latitude);
-            // var longitude = parseInt(one_event.longitude);
-
-            // var latlng = [one_event.latitude, one_event.longitude];
-            
-
-            // console.log(events);
-            // console.log(arrayOffullDate);
-            // console.log(events[key].fullDate);
 
             // Define the markers
-
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng(one_event.latitude, one_event.longitude),
                 map: map,
