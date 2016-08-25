2482c304855950f5eab721b11abd0ac46a87b4c8
Ling <lingsitu1290@gmail.com>
2016-08-25 06:31:43 +0000

    Have bar and line graph shell ready. Need to feed data in.

--- static/javascript/analyze.js
+++ static/javascript/analyze.js
@@ -3,6 +3,8 @@
 var options = { responsive: true };
 
 var ctx_donut = $("#donutChart").get(0).getContext("2d");
+var ctx_bar = $("#barChart").get(0).getContext("2d");
+var ctx_line = $("#lineChart").get(0).getContext("2d");
 
 // Make donut chart of percentage of different types of events for that day
 function makeDonut(){
@@ -18,4 +20,36 @@ function makeDonut(){
     });
 }
 
+// Make bar graph of number of protests in specific countries 
+function makeBar(){
+    var month = $("#month").val();
+    var day = $("#day").val();
+    $.get('/eventcode/' + '2016' + month + day + '.json', function (data) {
+        var myBarChart = new Chart(ctx_bar, {
+                                            type: 'bar',
+                                            data: data,
+                                            options: options
+                                          });
+        $('#donutLegend').html(myBarChart.generateLegend());
+    });
+}
+
+// Make line graph of all protests in 2016 
+function makeLine(){
+    var month = $("#month").val();
+    var day = $("#day").val();
+    $.get('/eventcode/' + '2016' + month + day + '.json', function (data) {
+        var myLineChart = new Chart(ctx_line, {
+                                          type: 'line',
+                                          data: data,
+                                          options: options
+                                        });
+        $('#donutLegend').html(myLineChart.generateLegend());
+    });
+}
+
+
+
 document.getElementById("submit").addEventListener("click", makeDonut);
+document.getElementById("submit").addEventListener("click", makeBar);
+document.getElementById("submit").addEventListener("click", makeLine);
