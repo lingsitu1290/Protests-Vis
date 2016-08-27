d1bf69ad3e89cc3d4c0434e29cedc951fc864b30
Ling <lingsitu1290@gmail.com>
2016-08-27 22:05:06 +0000

    Added test foranalyze route.

--- test_server.py
+++ test_server.py
@@ -8,6 +8,15 @@ class MyAppIntegrationTestCase(unittest.TestCase):
         client = server.app.test_client()
         result = client.get('/')
         self.assertIn('<div id="map-canvas"/> </div>', result.data)
+        self.assertEqual(result.status_code, 200)
+
+    def test_analyze(self):
+        client = server.app.test_client()
+        result = client.get('/analyze')
+        self.assertIn('<canvas id="barChart"></canvas>', result.data)
+        self.assertIn('<canvas id="donutChart"></canvas>', result.data)
+        self.assertEqual(result.status_code, 200)
+
 
 if __name__ == '__main__':
     # If script is called, run our tests
